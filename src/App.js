b126dbb10d3f4a746cfdf0f0e5d8d231069053f4
TulkasAstaldo <p.ioan9@gmail.com>
2020-04-30 22:37:07 +0300

    fixed look

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import React from "react"
-import useGame from "./hooks/useGame"
+import React from "react";
+import useGame from "./hooks/useGame";
 
-export default function App(){
-    const {
-        text, 
-        inputRef, 
-        timer, 
-        isTimeRunning, 
-        wordCount, 
-        handleChange, 
-        gameStart
-    } = useGame()
- 
+export default function App() {
+  const {
+    text,
+    inputRef,
+    timer,
+    isTimeRunning,
+    wordCount,
+    handleChange,
+    gameStart,
+  } = useGame();
 
-    return (
-        <main>
-            <h1>How Fast Can You Type?</h1>
-            <form>
-                <textarea 
-                    ref={inputRef}
-                    value={text}
-                    onChange={handleChange}
-                    disabled={!isTimeRunning}
-                    placeholder={isTimeRunning ? "" : "Press Start and type!"}
-                />
-            </form>
-            <h4>Time Remaining: {timer}</h4>
-            <button 
-                disabled={isTimeRunning} 
-                onClick={gameStart}
-            >
-                Start
-            </button>
-            <h1>Word Count: {wordCount}</h1>
-        </main>
-    )
+  return (
+    <main>
+      <h1>How Fast Can You Type?</h1>
+      <form>
+        <textarea
+          ref={inputRef}
+          value={text}
+          onChange={handleChange}
+          disabled={!isTimeRunning}
+          placeholder={isTimeRunning ? "" : "Press Start and type!"}
+        />
+      </form>
+      <h4>Time Remaining: {timer}</h4>
+      <button disabled={isTimeRunning} onClick={gameStart}>
+        Start
+      </button>
+      <h1>Word Count: {wordCount}</h1>
+    </main>
+  );
 }
